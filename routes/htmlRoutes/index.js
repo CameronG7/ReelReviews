b61f6af76d408de00cf178f13d7bcb3cda6500df
Marcus Jonas <marcusjonas@Marcuss-MacBook-Pro.local>
2023-05-10 21:59:47 -0700

    created views for home, singin and signup using bootstrap and handlebars to manipulate html. Connected to buttons and to basic routes for the three pages

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,1 +1,30 @@
 const router = require('express').Router();
+
+// /home routes to the home page
+router.get('/home', async (req, res) => {
+    try {
+      res.render('home', {});
+    } catch (error) {
+      res.status(500).json({error});
+    }
+  });
+// /signin routes to the sign-in page
+router.get('/signin', async (req, res) => {
+  try {
+    res.render('signin', {});
+  } catch (error) {
+    res.status(500).json({error});
+  }
+  });
+
+// /signup routes to the sign-up page
+router.get('/signup', async (req, res) => {
+  try {
+    res.render('signup', {});
+  } catch (error) {
+    res.status(500).json({error});
+  }
+});
+
+module.exports = router;
+
